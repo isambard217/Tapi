c76186251e82e72d505cde5c62103ba07300663e
Alhaytham <asalhaytham@gmail.com>
2018-07-01 00:09:19 +0100

    testing that the appllication context is creating controllers (i.e. routes handlers)

--- src/test/java/com/ops/base/RestBluePrint/RestBluePrintApplicationTests.java
+++ src/test/java/com/ops/base/RestBluePrint/RestBluePrintApplicationTests.java
@@ -1,15 +1,23 @@
 package com.ops.base.RestBluePrint;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.ops.base.RestBluePrint.routes.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RestBluePrintApplicationTests {
-
+	@Autowired
+	private UserRoute userRoute;
+	@Autowired TechniqueRoute techniquesRoute;
 	@Test
-	public void contextLoads() {}
+	public void contextLoads() throws  Exception{
+	    assertThat(userRoute).isNotNull();
+	    assertThat(techniquesRoute).isNotNull();
+    }
 
 }
