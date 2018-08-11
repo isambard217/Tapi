9d4ec057fb34d62e5c2e4574ea31cd8fd242175f
Alhaytham Elhassan <asalhaytham@gmail.com>
2018-08-12 00:28:38 +0100

    fix swagger config (#6)

--- src/main/java/com/ops/base/EducationProject/SwaggerConfig.java
+++ src/main/java/com/ops/base/EducationProject/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
 	public Docket api() {                
 	    return new Docket(DocumentationType.SWAGGER_2)          
 	      .select()                                       
-	      .apis(RequestHandlerSelectors.basePackage("com.ops.base.RestBluePrint.routes"))
+	      .apis(RequestHandlerSelectors.basePackage("com.ops.base.EducationProject.routes"))
 	      .paths(PathSelectors.ant("/api/*"))                     
 	      .build();
 	}
